bc54165adbaa2f0742f167ae5ce7971252add346
ubitinonevami <bernastya@inbox.ru>
2021-08-07 15:58:20 +0300

    d

--- itjobs/deploy.sh
+++ itjobs/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f  git@github.com:ubitinonevami/devjobs.git main:gh-pages
+git push -f  git@github.com:ubitinonevami/it-jobs.git main:gh-pages
 cd -
